ba3c9191e62cfc38b246d4ca9bef5e892f410afe
saeed <saeed.da1994@gmail.com>
2023-02-02 18:10:47 +0330

    add some functionality

--- js/main.js
+++ js/main.js
@@ -3,21 +3,35 @@ function createModal(config) {
   //create modal
   const modal = document.createElement("div");
   modal.classList.add("modal");
+  //create modal background
+  const modalBg = document.createElement("div");
+  modalBg.classList.add("background");
+  modal.appendChild(modalBg);
   //create modal container
   const modalContainer = document.createElement("div");
   modalContainer.classList.add("modal-container");
-  modal.appendChild(modalContainer);
   //create modal header
   const modalHeader = document.createElement("div");
   modalHeader.classList.add("header");
   modalHeader.innerHTML = config.title;
   modalContainer.appendChild(modalHeader);
+  // create close btn
+  const btn = document.createElement("button");
+  btn.innerHTML = "&times;";
+  btn.classList.add("pointer");
+  modalHeader.appendChild(btn);
+  // close modal functionality
+  btn.onclick = () => {
+    modal.remove();
+  };
   //create modal body
   const modalBody = document.createElement("div");
   modalBody.classList.add("body");
   modalBody.innerHTML = config.message;
   modalContainer.appendChild(modalBody);
-  document.querySelector("body").appendChild(modal);
+  modal.appendChild(modalContainer);
+  document.body.appendChild(modal);
+  
 }
 
 createModal({
